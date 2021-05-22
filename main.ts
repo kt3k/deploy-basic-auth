d7bdf149f0d1f990e8da04e63b1029ca69f747a3
Yoshiya Hinosawa <stibium121@gmail.com>
2021-05-22 19:22:23 +0900

    fix: return 401 when user:password doesn't match

--- main.ts
+++ main.ts
@@ -1,42 +1,27 @@
-
 const userPasswordTable = {
   user: "password",
 }
+
 addEventListener("fetch", (e: FetchEvent) => {
   const authorization = e.request.headers.get("authorization");
-
-  if (!authorization) {
-    e.respondWith(new Response("", {
-      status: 401,
-      statusText: "Unauthorized",
-      headers: {
-	"www-authenticate": `Basic realm="Access to the metrics"`,
-      },
-    }));
-    return;
-  }
-
-  const m = authorization.match(/^Basic\s+(.*)$/);
-  if (!m) {
-    e.respondWith(new Response("Forbidden", {
-      status: 403,
-      statusText: "Forbidden",
-    }));
-    return;
-  }
-
-  const cred = atob(m[1]);
-  const [user, pw] = cred.split(":");
-
-  for (const [u, p] of Object.entries(userPasswordTable)) {
-    if (user === u && pw == p) {
-      e.respondWith(new Response(`hello ${id}!`));
-      return;
+  if (authorization) {
+    const match = authorization.match(/^Basic\s+(.*)$/);
+    if (match) {
+      const [user, pw] = atob(match[1]).split(":");
+      for (const [u, p] of Object.entries(userPasswordTable)) {
+        if (user === u && pw == p) {
+          e.respondWith(new Response(`hello ${id}!`));
+          return;
+        }
+      }
     }
   }
 
-  e.respondWith(new Response("Forbidden", {
-    status: 403,
-    statusText: "Forbidden",
+  e.respondWith(new Response("", {
+    status: 401,
+    statusText: "Unauthorized",
+    headers: {
+      "www-authenticate": `Basic realm="Access to the metrics"`,
+    },
   }));
 });
